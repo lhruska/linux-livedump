0fcca37d0b9cd1e9614ed8d5df23f61268c3c704
Lukas Hruska <lukyhruska96@gmail.com>
2024-12-17 00:41:20 +0100

    livedump: kfifo_debug script now shows time as ns since start of the test
    Signed-off-by: Lukas Hruska <lukyhruska96@gmail.com>

--- tools/livedump/kfifo_debug.sh
+++ tools/livedump/kfifo_debug.sh
@@ -8,7 +8,9 @@ while [ "$(get_state)" != "1" ]; do
 	sleep 1
 done
 
+starttime="$(date +%s%N)"
+
 echo "state;time_ns;page_fault_pending;page_fault_inserted;page_fault_finished;sweep_pending;sweep_inserted;sweep_finished"
 while [ "$(get_state)" != "5" ]; do
-	cat /sys/kernel/livedump/kfifo_debug  | cut -f 2 -d ':' | tr -d ' ' | awk -v timestamp="$(date +%s%N)" 'NR==1{state=$0; next} NR==3{line=$0; next} NR==4{print state ";" timestamp ";" line ";" $0; ext}' | tr ',' ';'
+	cat /sys/kernel/livedump/kfifo_debug  | cut -f 2 -d ':' | tr -d ' ' | awk -v timestamp="$(date +%s%N)" -v starttime=$starttime 'NR==1{state=$0; next} NR==3{line=$0; next} NR==4{print state ";" timestamp-starttime ";" line ";" $0; ext}' | tr ',' ';'
 done
